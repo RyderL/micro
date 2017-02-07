6d6d6483aec67a2a74b58d605323be371a4fcb45
chaomeng.zhang <zcm101@163.com>
2017-02-07 11:08:26 +0800

    注解方式的负载均衡 增加ribbon策略配置

--- console/src/main/java/com/micro/console/config/RestfulConfig.java
+++ console/src/main/java/com/micro/console/config/RestfulConfig.java
@@ -1,25 +1,25 @@
 package com.micro.console.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestfulConfig {
 
     @Bean
-    @ExceptionHandler
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate(clientHttpRequestFactory());
     }
 
     private ClientHttpRequestFactory clientHttpRequestFactory() {
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setReadTimeout(2000);
-        factory.setConnectTimeout(2000);
+        factory.setReadTimeout(2000); // 可以根据生产调整
+        factory.setConnectTimeout(2000); // 可以根据生产调整
         return factory;
     }
 }
--- console/src/main/java/com/micro/console/utils/UriUtil.java
+++ console/src/main/java/com/micro/console/utils/UriUtil.java
@@ -13,16 +13,30 @@ public class UriUtil {
     @Resource
     private LoadBalancerClient loadBalancerClient;
 
+//    /**
+//     * 根据实例名获取其中一个具体实例
+//     * @param serverName 客户端实例名
+//     * @return
+//     */
+//    public String getUri(String serverName) {
+//        URI uri = null;
+//        try {
+//            ServiceInstance instance = this.loadBalancerClient.choose(serverName);
+//            uri = instance.getUri();
+//        } catch (Exception e) {
+//            //当服务不可用时
+//            e.printStackTrace();
+////            uri = URI.create(fallbackUri);
+//        }
+//        return uri.toString();
+//    }
+
+    /**
+     * 按实例名访问
+     * @param serverName 客户端实例名
+     * @return
+     */
     public String getUri(String serverName) {
-        URI uri = null;
-        try {
-            ServiceInstance instance = this.loadBalancerClient.choose(serverName);
-            uri = instance.getUri();
-        } catch (Exception e) {
-            //当服务不可用时
-            e.printStackTrace();
-//            uri = URI.create(fallbackUri);
-        }
-        return uri.toString();
+        return "http://" + serverName;
     }
 }
