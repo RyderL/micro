fe00a069a267957ebd09c661def999f1e8ea3ddc
chaomeng.zhang <zcm101@163.com>
2017-04-17 13:47:24 +0800

    最简spring boot

--- boot/src/main/java/com/micro/boot/config/Application.java
+++ boot/src/main/java/com/micro/boot/config/Application.java
@@ -1,45 +1,15 @@
 package com.micro.boot.config;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.Environment;
-import org.springframework.util.StringUtils;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @ComponentScan(basePackages = {"com.micro"})
-@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
-    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
-
-    public static void main(String[] args) throws UnknownHostException {
-        SpringApplication app = new SpringApplication(Application.class);
-        Environment env = app.run(args).getEnvironment();
-        String contextPath = env.getProperty("server.context-path");
-        if (StringUtils.isEmpty(contextPath)) {
-            LOG.info("Access URLs:\n----------------------------------------------------------\n\t" +
-                            "Local: \t\thttp://127.0.0.1:{}\n\t" +
-                            "External: \thttp://{}:{}\n----------------------------------------------------------",
-                    env.getProperty("server.port"),
-                    InetAddress.getLocalHost().getHostAddress(),
-                    env.getProperty("server.port"));
-        } else {
-            LOG.info("Access URLs:\n----------------------------------------------------------\n\t" +
-                            "Local: \t\thttp://127.0.0.1:{}{}\n\t" +
-                            "External: \thttp://{}:{}{}\n----------------------------------------------------------",
-                    env.getProperty("server.port"),
-                    env.getProperty("server.context-path"),
-                    InetAddress.getLocalHost().getHostAddress(),
-                    env.getProperty("server.port"),
-                    env.getProperty("server.context-path"));
-        }
+    public static void main(String[] args) {
+        new SpringApplication(Application.class).run(args);
     }
 }
